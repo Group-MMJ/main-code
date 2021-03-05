359ef00ed65403a1602cd636cb7b12c010c72779
Janusz Snieg <25085325@students.lincoln.ac.uk>
2021-03-05 18:31:30 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    //zie plan
+    /*D'Hondt method
+        //first round - party with the most votes wins a seat
+        //winning party's vote is divided by 2
+        //whichever party comes out on top 
+        //re-ordered results
+        //it's looped
+        //original vote of the winning party in each round
+        //divide by 1 and + total of running MEPs
+        //while loop -> until all the seats for the region have been taken*/
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadFrom object1 = new ReadFrom();
+            object1.read_s();
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
---
+++ ReadFrom.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class ReadFrom
+    {
+        public void read_s()
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"D:\University of Lincoln\SEM B\Assessment1Data.txt");
+            //12 lines total
+            foreach (string line in lines)
+            {
+                Console.WriteLine("\t" + line);
+            }
+            Console.WriteLine("\n");
+
+            //prints entire line if it finds a keyword
+            var fileName = @"D:\University of Lincoln\SEM B\Assessment1Data.txt";
+            foreach (var line in File.ReadLines(fileName))
+            {
+                if (line.Contains("Labour"))
+                {
+                    Console.WriteLine("test: {0}", line);
+                    break;
+                }
+            }
+        }
+    }
+}
