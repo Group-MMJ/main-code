17062cbaee504beb6464b7008edf9410c38c063b
Janusz Snieg <25085325@students.lincoln.ac.uk>
2021-03-09 18:07:21 +0000

    first round and second round right way of doing it

--- dHondt.cs
+++ dHondt.cs
@@ -18,6 +18,9 @@ namespace ConsoleApp1
             ReadFrom object1 = new ReadFrom();
             object1.read_s();
 
+            string[] partyNames = { "Brexit Party", "Liberal Democrats", "Labour", "Conservative", "Green", "UKIP", "Change UK", 
+            "Independent Network","Independent" };
+
             Console.Write("\nGive number of votes for a party: ");
             numseat = Convert.ToInt32(Console.ReadLine());
 
@@ -27,21 +30,24 @@ namespace ConsoleApp1
             roundnum = Convert.ToInt32(Console.ReadLine());
 
             int numend = (numseat / 1) + mepnum;
+            Console.WriteLine($"Round : 1 \tVotes : {(numseat / 2) + mepnum}"); //First round is dividing by 2
 
-            int x = 1;
+            int x = 2;
             while (x <= roundnum)
             {
+                
                 for (int h = 1; h < roundnum; h++)
                 {
-                    numend = (numseat / 2) + mepnum;
+                    numend = (numseat / 1) + mepnum;
                 }
-                Console.WriteLine($"Round : {x} \tFinal number : {numend}");
+                Console.WriteLine($"Round : {x} \tVotes : {numend}");
                 x++;
             } 
 
             //saves the result into a new file
 
         }
+
     }
 }
 
